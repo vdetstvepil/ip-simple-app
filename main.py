4b17ffef32841143ef9ea5a06ccf99fc20cafefc
Anton Kislyakov <antonkis2012@gmail.com>
2025-05-13 23:16:19 +0300

    Добавлено чтение текста из input.txt

---
+++ main.py
@@ -0,0 +1,13 @@
+def main():
+    input_path = "input.txt"
+
+    try:
+        with open(input_path, "r", encoding="utf-8") as f:
+            text = f.read()
+        print("Файл успешно прочитан")
+    except FileNotFoundError:
+        print(f"Файл {input_path} не найден.")
+        return
+
+if __name__ == "__main__":
+    main()
