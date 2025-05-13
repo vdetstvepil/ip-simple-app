6d3065a987705fd2e98a86b9f42d3c6a6b0ea59e
Anton Kislyakov <antonkis2012@gmail.com>
2025-05-13 23:18:04 +0300

    Добавлена запись результата в output.txt

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ def main():
             print("Файл успешно прочитан")
             updated_text = replace_text_numbers(text)
             print("Обработка завершена")
+
+        output_path = "output.txt"
+        with open(output_path, "w", encoding="utf-8") as f:
+            f.write(updated_text)
+        print(f"✅ Готово! Результат записан в {output_path}")
+
     except FileNotFoundError:
         print(f"Файл {input_path} не найден.")
         return
